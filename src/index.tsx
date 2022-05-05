4776ba55d70d79a0cf509133979daba1f4b67a37
Taichi Komoto <724komodora915@gmail.com>
2022-05-06 07:35:36 +0900

    add theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from './theme/theme';
+
 import App from './components/App';
 
 const rootElement = document.getElementById('root');
@@ -8,7 +11,9 @@ if (rootElement) {
   const root = createRoot(rootElement);
   root.render(
     <StrictMode>
-      <App />
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
     </StrictMode>
   );
 }
